9a7f99cbc02dc85e9bc674d6bbff748fa646e33e
Fayne Aldan <faynealdan@gmail.com>
2020-04-18 02:22:42 -0600

    Some changes

--- devCCModLoader.js
+++ devCCModLoader.js
@@ -6,101 +6,59 @@ if (CCModLoader === undefined) var CCModLoader = {};
 if (typeof CCSE == "undefined")
   Game.LoadMod("https://klattmose.github.io/CookieClicker/CCSE.js");
 CCModLoader.name = "CC Mod Loader";
-CCModLoader.version = "1.0";
+CCModLoader.version = "1.0-dev";
 CCModLoader.GameVersion = "2.022";
-CCModLoader.klattmose = [
-  {
-    name: "Fortune Cookie",
-    desc: "Predicts the outcomes of the Grimoire minigame spells.",
-    url: "https://klattmose.github.io/CookieClicker/FortuneCookie.js",
-  },
-  {
-    name: "Klattmose Utilities",
-    desc: "Create an unlimited amount of fully customizable hotkeys.",
-    url: "https://klattmose.github.io/CookieClicker/KlattmoseUtilities.js",
-  },
-  {
-    name: "Horticookie",
-    desc: "A helper tool for the Garden minigame.",
-    url: "https://klattmose.github.io/CookieClicker/Horticookie.js",
-  },
-  {
-    name: "Casino",
-    desc:
-      "Adds a minigame to the Chancemaker building where you can play blackjack and bet cookies.",
-    url: "https://klattmose.github.io/CookieClicker/minigameCasino.js",
-  },
-  {
-    name: "American Season",
-    desc: "Adds a firework themed season to the game.",
-    url: "https://klattmose.github.io/CookieClicker/AmericanSeason.js",
-  },
-  {
-    name: "Timer Widget",
-    desc:
-      "Adds a special object in the lower left (near Krumblor and Santa) that displays a countdown for buffs and shimmers.",
-    url: "https://klattmose.github.io/CookieClicker/CCSE-POCs/TimerWidget.js",
-  },
-  {
-    name: "Hurricane Sugar",
-    desc:
-      "Adds a Golden Cookie effect that briefly shortens the time for sugar lumps to ripen to 1 second.",
-    url:
-      "https://klattmose.github.io/CookieClicker/CCSE-POCs/HurricaneSugar.js",
-  },
-  {
-    name: "Black Hole Inverter",
-    desc:
-      "Adds a new building and an appropriate amount of upgrades and achievments.",
-    url:
-      "https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js",
-  },
-];
 
 CCModLoader.launch = function () {
   CCModLoader.init = function () {
-    CCModLoader.isLoaded = 1;
+    CCModLoader.isLoaded = true;
     CCModLoader.modCount = 0;
-    CCModLoader.enabled = {};
-    CCModLoader.loadedMods = {};
+    CCModLoader.config = {};
+    CCModLoader.loadedMods = [
+      "https://klattmose.github.io/CookieClicker/CCSE.js",
+    ];
 
-    CCSE.customSave.push(function () {
-      CCSE.save.OtherMods.CCModLoader = CCModLoader.config;
-    });
-    CCSE.customLoad.push(function () {
-      if (CCSE.save.OtherMods.CCModLoader)
-        CCModLoader.config = CCSE.save.OtherMods.CCModLoader;
-      else CCModLoader.config = CCModLoader.defaultConfig();
-    });
+    CCModLoader.loadConfig();
+    CCSE.customLoad.push(CCModLoader.loadConfig);
+    CCSE.customSave.push(CCModLoader.saveConfig);
+
+    CCModLoader.loadMods();
 
     CCModLoader.ReplaceGameMenu();
     CCModLoader.countNotif();
   };
 
+  CCModLoader.loadMods = function () {
+    if (!CCModLoader.config.autoLoad) return;
+    for (var mod of CCModLoader.config.mods)
+      if (mod.enabled) CCModLoader.loadMod(mod.url);
+  };
+
+  CCModLoader.loadMod = function (url) {
+    if (CCModLoader.loadedMods.indexOf(url) >= 0) return;
+    Game.LoadMod(url);
+    CCModLoader.modCount++;
+    CCModLoader.loadedMods.push(url);
+  };
+
+  CCModLoader.saveConfig = function () {
+    CCSE.save.OtherMods.CCModLoader = CCModLoader.config;
+  };
+
+  CCModLoader.loadConfig = function () {
+    CCModLoader.config = CCModLoader.defaultConfig();
+    if (CCSE.save.OtherMods.CCModLoader) {
+      var config = CCSE.save.OtherMods.CCModLoader;
+      for (var pref in config) {
+        CCModLoader.config[pref] = config[pref];
+      }
+    }
+  };
+
   CCModLoader.defaultConfig = function () {
     return {
-      enabled: true,
-      mods: [
-        {
-          enabled: false,
-          name: "Cookie Monster",
-          url: "https://aktanusa.github.io/CookieMonster/CookieMonster.js",
-        },
-        {
-          enabled: 0,
-          name: "Cookie Monster Beta",
-          url: "https://aktanusa.github.io/CookieMonster/CookieMonsterBeta.js",
-        },
-        {
-          enabled: 0,
-          extraDelay: 0,
-          isLoaded: 0,
-          name: "Cookie Monster Dev",
-          url:
-            "https://cdn.rawgit.com/Aktanusa/CookieMonster/dev/CookieMonster.js",
-          waitForScriptLoad: 0,
-        },
-      ],
+      autoLoad: true,
+      mods: [],
     };
   };
 
@@ -131,7 +89,7 @@ CCModLoader.launch = function () {
     });
   };
 
-  CCModLoader.getOptionsMenu() = function () {
+  CCModLoader.getOptionsMenu = function () {
     var WriteButton = function (patchName, button, on, off, callback, invert) {
       var invert = invert ? 1 : 0;
       if (!callback) callback = "";
@@ -147,7 +105,7 @@ CCModLoader.launch = function () {
     var notif = "Loading " + count + " mod...";
     if (count > 1) notif = "Loading " + count + " mods...";
     if (Game.prefs.popups) Game.Popup(notif);
-    else Game.Notify("CC Mod Loader", notif, [16, 5], 5);
+    else Game.Notify(CCModLoader.name, notif, [16, 5], 5);
   };
 
   if (
